1556be4040d3d30fbdf65b617e921544ab55065c
iptaylortechnical <iptaylortechnical@gmail.com>
2015-10-06 19:21:03 -0600

    engine work

--- engine/engine.py
+++ engine/engine.py
@@ -1,1 +1,7 @@
 __author__ = 'Isaiah'
+
+class engine:
+
+    def __init__(self, users, games, slots):
+        if not users % slots == 0:
+            raise ValueError("users and slots not divisible")
